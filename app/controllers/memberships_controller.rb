3a2db632fa5371f596263fd0981c0470d8cf2fed
Tom Dyer <tdyer@ourstage.com>
2014-01-30 19:35:01 -0500

    Allow students to update their final grade for a course.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,11 @@
 class MembershipsController < ApplicationController
 
+def update
+  membership = Membership.find(params[:id])
+  membership.update_attributes(final_grade: membership_params[:final_grade])
+  redirect_to :back
+end
+
 def create
   Membership.create!(membership_params)
   redirect_to :back
@@ -7,6 +13,6 @@ end
 private
 
   def membership_params
-    params.require(:membership).permit(:student_id, :course_id)
+    params.require(:membership).permit(:student_id, :course_id, :final_grade)
   end
 end

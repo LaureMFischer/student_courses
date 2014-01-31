921279a4ebc0ff50340c6cd1b6397c00672bdb51
Tom Dyer <tdyer@ourstage.com>
2014-01-31 03:18:44 -0500

    Prevent student from being added twice to a course.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,13 @@ class Course < ActiveRecord::Base
   has_many :memberships
   has_many :students, through: :memberships
 
+  # students that can be added.
+  # don't want to add student more than once
+  def students_to_add
+    Student.all - self.students
+  end
+
+  def can_add_students?
+    !self.students_to_add.empty?
+  end
 end
